ceab4a145a8b5539fa84f6d68a1006e44ae39605
hunght37 <104755302+hunght37@users.noreply.github.com>
2022-08-27 12:42:11 +0700

    Add files via upload

---
+++ localstrogre form đăng ký.js
@@ -0,0 +1,25 @@
+localStorage.setItem('user','hunght37')
+localStorage.setItem('password','hunght37')
+localStorage.setItem('password-repeat','hunght37')
+function validate() {
+    var u = document.getElementById("username").value;
+    var p1 = document.getElementById("password").value;
+    var p2 = document.getElementById("password-repeat").value;
+     
+    if(u== "") {
+    alert("Vui lòng nhập tên!");
+    return false;
+    }
+    if(p1 == "") {
+    alert("Vui lòng nhập mật khẩu!");
+    return false;
+    }
+    if(p2 == "") {
+    alert("Vui lòng xác minh mật khẩu!");
+    return false;
+    }
+     
+    alert("Xin hãy điền đúng thông tin!")
+     
+    return true;
+    }
